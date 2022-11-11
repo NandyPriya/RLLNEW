2e56946fba2213d5909d90633318a800bee4982a
Nandhinipriya <nandhinipriya745@gmail.com>
2022-11-11 16:59:15 +0530

    change the location

---
+++ PROD/Models/CARRENT.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PROD.Models
+{
+    public class CARRENT
+    {
+        public int RentId { get; set; }
+        public Nullable<int> CarId { get; set; }
+        public Nullable<int> CustomerId { get; set; }
+        
+        public DateTime RentOrderDate { get; set; }
+        public Nullable<int> OdoReading { get; set; }
+        
+        public Nullable<System.DateTime> ReturnDate { get; set; }
+
+        public Nullable<int> ReturnOdoReading { get; set; }
+        public string LicenseNumber
+        {
+            get;
+            set;
+        }
+
+
+    }
+    public class searchdates
+    {
+
+        [DataType(DataType.Date)]
+        public DateTime RentDate
+        {
+            get;
+            set;
+        }
+        [DataType(DataType.Date)]
+        public DateTime ReturnDate
+        {
+            get;
+            set;
+        }
+    }
+}
